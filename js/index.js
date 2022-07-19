2802920bcde9cace39ba5223f5fa703d58a839fc
Bhavish <bhavishslc@gmail.com>
2022-07-19 15:18:09 +0530

    fixed nav issue

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@
 const header = document.getElementsByClassName('header')[0];
 const hamburger = document.getElementsByClassName('hamburger')[0];
 const action_container = document.getElementsByClassName('action-container')[0];
+const about_btn = document.getElementById('about_btn');
+const blogs_btn = document.getElementById('blogs_btn');
+const contact_btn = document.getElementById('contact_btn');
+const projects_btn = document.getElementById('projects_btn');
+const hire_btn = document.getElementById('hire_btn');
+
+
+
 let is_hamburger_open = false;
 
 hamburger.addEventListener('click', () => {
@@ -23,3 +31,19 @@ hamburger.addEventListener('click', () => {
     }
 }
 );
+
+
+function closeNavWindow() {
+  is_hamburger_open = false;
+  header.style.backgroundColor = '#fff';
+  header.childNodes[1].style.color = '#000';
+  action_container.style.visibility = 'hidden';
+  hamburger.childNodes[0].src='assets/icons/hamburger_black.svg';
+}
+about_btn.addEventListener('click',closeNavWindow); 
+blogs_btn.addEventListener('click',closeNavWindow);
+projects_btn.addEventListener('click',closeNavWindow);
+contact_btn.addEventListener('click',closeNavWindow);
+hire_btn.addEventListener('click',closeNavWindow);
+
+
